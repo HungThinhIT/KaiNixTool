3712451e452c414ab42bfdc761cd6544979d0ea7
HungThinh0710 <nhtnokia@gmail.com>
2020-07-20 09:21:52 +0700

    tmp change

--- controllers/request.js
+++ controllers/request.js
@@ -1,4 +1,4 @@
-const { net } = require('electron')
+const axios = require('axios');
 
 const store = global.share.store;
 const ipcMain = global.share.ipcMain;
@@ -17,7 +17,7 @@ const ipcMain = global.share.ipcMain;
 var responseData = {
     statusCode: null,
     header: null,
-    data: null,
+    data: null, //contain chunked-body-data
 }
 
 /*
@@ -33,7 +33,8 @@ var responseData = {
 */
 
 ipcMain.on('prepare-api-request', async(event, api) => {
-    console.log(api);    
+    console.log(api);
+
     /*
     * STRUCT_API:
     *     id: idApi,
@@ -41,33 +42,80 @@ ipcMain.on('prepare-api-request', async(event, api) => {
     *     api: 'jsonplaceholder.typicode.com/albums/1/photos',
     *     method: 'get'
     */
-    const request = net.request({
-        method: 'GET',
-        protocol: 'https:',
-        hostname: 'jsonplaceholder.typicode.com/albums/1/photos',
-        // port: 443,
-        path: '/'
-    })
 
-    request.on('response', (response) => {
-        // let dataResponse;
-        console.log(`STATUS: ${response.statusCode}`)
-        console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
-        console.log(response);
-        // console.log(response.data);
+    /**
+     * DEBUG_API
+     */
+    apiDev = {
+        method: 'get',
+        url: 'http://localhost:3000/user',
+        authenticate: {
+            isAuth: true,
+            type: 'Bearer',
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk1MDY0MjQwfQ.Y2jdwG8G1JHJOD4woXMrOOohNg7aY2zKVRQV3-_kqGo',
+        },
+        data: {},
+    }
+   try {
+    const request = await axios({
+        method: apiDev.method,
+        url: apiDev.url,
+        data: apiDev.data,
+        headers: {
+            Authorization: `Bearer ${apiDev.authenticate.token}`,
+        }
+    })
+      console.log("DATA_IN_[request.js]");
+      console.log(request);
+      console.log(request.status);
+      console.log(request.data);
+      console.log("END_THEN");
+   } catch (error) {
+       console.log("ERROR_IN_[request.js]_execute api");
+       console.log(error);
+       console.log(error.message);
+       console.log(error.response.data);
+       console.log(error.response.status);
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+    // const request = net.request({
+    //     method: 'GET',
+    //     protocol: 'https:',
+    //     hostname: 'jsonplaceholder.typicode.com/albums/1/photos',
+    //     // port: 443,
+    //     path: '/'
+    // })
+
+    // request.on('response', (response) => {
+    //     // let dataResponse;
+    //     console.log(`STATUS: ${response.statusCode}`)
+    //     console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+    //     console.log(response);
+    //     // console.log(response.data);
         
-        response.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`)
-            console.log(chunk);
-            responseData.statusCode = response.statusCode
-            responseData.header = response.headers
-            responseData.data = JSON.parse(chunk.toString())
-            event.returnValue = responseData
-        })
-        response.on('end', () => {
-          console.log('No more data in response.')
-        })
-      })
-    request.end()
+    //     response.on('data', (chunk) => {
+    //         console.log(`BODY: ${chunk}`)
+    //         console.log(chunk);
+    //         responseData.statusCode = response.statusCode
+    //         responseData.header = response.headers
+    //         responseData.data = JSON.parse(chunk.toString())
+    //         event.returnValue = responseData
+    //     })
+    //     response.on('end', () => {
+    //       console.log('No more data in response.')
+    //     })
+    //   })
+    // request.end()
     
 })
