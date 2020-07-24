2faea96343d90903edd47adbc3e87b4f5c368dbe
SinJunior <khtuankendy@gmail.com>
2020-07-24 22:09:30 +0700

    fix url.js

--- resources/custom/js/url.js
+++ resources/custom/js/url.js
@@ -3,7 +3,7 @@ function checkSpecialSymbol(input) {
 	if (val.length > 1) {
 		if (val.includes("?")) {
 			if (val.substr(0, 1) != "?") {
-				var ls = val.spit("?");
+				var ls = val.split("?");
 				if (ls[0].length > 1) {
 					if (ls[0].substr(ls[0].length - 1, 1) == "/") {
 						val = ls[0].substr(0, ls[0].length - 1) + "?" + ls[1]; 
