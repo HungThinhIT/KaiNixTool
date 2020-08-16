0d7b3c52c408670b411763e50aafd0b1c8d859c2
SinJunior <khtuankendy@gmail.com>
2020-08-16 21:36:21 +0700

    Fix duplicate when send request

--- preloads/main/main-screen-preload.js
+++ preloads/main/main-screen-preload.js
@@ -2,12 +2,13 @@ const ipcRenderer = require('electron').ipcRenderer;
 const dateFormat = require('dateformat');
 
 
-window.addEventListener('click', () => {
+window.addEventListener('DOMContentLoaded', () => {
     
     var tab = document.getElementById('tab-tab-item');
     const tabOld = tab.childNodes[0];
     var pane = document.getElementById('pcntm');
     const oldPane = pane.cloneNode(true);
+    const sendButton = document.getElementById('send');
     /*
     |-----------------------------------------------
     | DOM Data from FE and send it via IPC
@@ -19,223 +20,226 @@ window.addEventListener('click', () => {
     |
     |
     */
-    document.querySelector('#send').addEventListener('click', async () => { //NOT YET    
-        var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
-        var titleResponse = document.getElementById('title-response');
-        var gifPreload = document.getElementsByClassName('gif-pre-load')[0];               
-        preloadResponse.style.display = 'block';
-        titleResponse.style.display = 'none';
-        gifPreload.style.display = 'block';
-        var errorEle = document.getElementById('error-log-response');
-        errorEle.style.display = 'none';
-        
-        var url = document.getElementsByName("path")[0].value;
-        console.log(url);
-        var ls = {};
-        if (url.length > 0) {
-            ls = url.split('?');
-            if (ls[1] != undefined) {
-                if (ls[0].length <= 0) url = "http://localhost?" + ls[1];
-                else if (ls[0].includes("https://") || ls[0].includes("http://")) {
-                    url = ls[0] + "?" + ls[1];
-                }
-            }
+    docReady(() => {
+        document.querySelector('#send').addEventListener('click', async () => { //NOT YET    
+            var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
+            var titleResponse = document.getElementById('title-response');
+            var gifPreload = document.getElementsByClassName('gif-pre-load')[0];               
+            preloadResponse.style.display = 'block';
+            titleResponse.style.display = 'none';
+            gifPreload.style.display = 'block';
+            var errorEle = document.getElementById('error-log-response');
+            errorEle.style.display = 'none';
             
-        }
-        else {
-            url = "http://localhost";
-        }
-    
-        var tbodyHeaders = document.getElementById("tbody-headers");
-        var tbodyBodys = document.getElementById("tbody-forms");
-        var oauthCheck = document.getElementsByName('authorize-type');
-        var methodRequest = document.getElementsByName("method-request");
-        var methodType = methodRequest[0].value;
-    
-        var authValue = oauthCheck[0].value;
-        var isAuthentication = false;
-        var authContent = "";
-        if (authValue != "none") {
-            if ("oauth-2".localeCompare(authValue) == 0) authValue = "Bearer";
-            isAuthentication = true;
-            authContent = document.getElementsByName("auth-token")[0].value;
-            if (!authContent.includes("Bearer")) {
-                authContent = authContent.trim();
-                if (authContent.length > 1) {
-                    var authList = authContent.split(/\s/g);
-                    authContent = "";
-                    authList.forEach( function(element, index) {
-                        authContent += element;
-                    });
-                }
-            }
-            else {
-                authContent = authContent.substr("Bearer ".length - 1, authContent.length - 1);
-                authContent = authContent.trim();
-                if (authContent.length > 1) {
-                    var authList = authContent.split(/\s/g);
-                    authContent = "";
-                    authList.forEach( function(element, index) {
-                        authContent += element;
-                    });
-                }
-            }
-        }
-    
-        // Authentication
-        var authen = {
-            type: authValue,
-            isAuthen: isAuthentication,
-            token: authContent
-        }
-    
-        // Header
-        var trHeadersParent = tbodyHeaders.childNodes;
-        var headersParams = {}
-        var countHeader = 0;
-        trHeadersParent.forEach( function(element, index) {
-            if (element.tagName == 'TR') {
-                var param = {}
-                var tdParams = element.childNodes;
-                var isCheck = tdParams[1].childNodes[1].checked;
-                var key = tdParams[3].childNodes[1].value;
-                var value = tdParams[5].childNodes[1].value;
-                var description = tdParams[7].childNodes[1].value;
-                if (isCheck == true) {
-                    headersParams[countHeader] = {
-                        isCheck: isCheck,
-                        key: key,
-                        value: value,
-                        description: description
+            var url = document.getElementsByName("path")[0].value;
+            console.log(url);
+            var ls = {};
+            if (url.length > 0) {
+                ls = url.split('?');
+                if (ls[1] != undefined) {
+                    if (ls[0].length <= 0) url = "http://localhost?" + ls[1];
+                    else if (ls[0].includes("https://") || ls[0].includes("http://")) {
+                        url = ls[0] + "?" + ls[1];
                     }
-                    countHeader++;
                 }
-            }
-        });
-        if (countHeader == 0) headersParams = {};
-    
-        // Body
-        var checkValueForm = "none";
-        var formCheck = document.getElementsByName('body-form');
-        formCheck.forEach( function(element, index) {
-            if (element.checked) {
-                checkValueForm = element.value;
-            }
-        });
-        var body = {}
-        if (checkValueForm != "none") {
-            if (checkValueForm == "form-data") {
-                body.type = checkValueForm;
-                var trBodyParent = tbodyBodys.childNodes;
-                var countBody = 0;
-                trBodyParent.forEach( function(element, index) {
-                    if (element.tagName == 'TR') {
-                        var tdParams = element.childNodes;
-                        var isCheck = tdParams[1].childNodes[1].checked;
-                        var key = tdParams[3].childNodes[1].value;
-                        var value = tdParams[5].childNodes[1].value;
-                        var description = tdParams[7].childNodes[1].value;
-                        if (isCheck == true) {
-                            body[countBody] = {
-                                isCheck: isCheck,
-                                key: key,
-                                value: value,
-                                description: description
-                            }
-                            countBody++;
-                        }
-                    }
-                });
-                if (countBody == 0) body = {};
-            }
-            else if (checkValueForm == "raw"){
-                body.type = checkValueForm;
                 
             }
-        }
-    
-        var data = { 
-            Req: {
-                Url: url,
-                Authen: authen,
-                Headers: headersParams,
-                Body: body,
-                Type: methodType
+            else {
+                url = "http://localhost";
             }
-        }
-    
-        // console.log(data);
-    
-        // Test preload response => 
-        // setTimeout(function() {
-        //     titleResponse.style.display = 'block';
-        //     gifPreload.style.display = 'none';
-        //     var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
-        //     preloadResponse.style.display = 'none';
-        // }, 1000);
-    
-        // Change above code by below code
-    
-        // titleResponse.style.display = 'block';
-        // gifPreload.style.display = 'none';
-        // var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
-        // preloadResponse.style.display = 'none';
-    
-    
-        // Test show response data 
-    
-        // var response = {
-        // 	header: {
-        // 		0: {
-        // 			key: "Content-type",
-        // 			value: "application/json"
-        // 		},
-        // 		1: {
-        // 			key: "Content-type",
-        // 			value: "application/json"
-        // 		},
-        // 		2: {
-        // 			key: "Content-type",
-        // 			value: "application/json"
-        // 		},
-        // 	},
-        // 	time: 100,
-        // 	size: 100,
-        // 	statusCode: 200,
-        // 	data: {data:"fa", fa:"data"},
-        // 	request: ""
-        // }
-        // responseData(response);
         
-        const date = dateFormat(new Date(), "yyyy-mm-dd");
-        const idApi = dateFormat(new Date(), "yyyymmddHHMMssL");
-        const timeRequest = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss:L"); //2020-06-01 15:04:16:09 -> example
-        //const isOpen = '' //TODO: WAITING UI is finish.
-        //const api = ''//TODO: WAITING UI is finish.
-        //const method = ''//TODO: WAITING UI is finish.
-        let api = {
-            date: date,
-            isOpen: true,
-            apiEndPoint: [{
-                id: idApi,
-                timeRequest: timeRequest,
-                url: data.Req.Url,
-                method: data.Req.Type,
-                headers: data.Req.Headers,
-                authenticate: data.Req.Authen,
-                body: data.Req.Body
-            }],
-        }
-        //
-        console.log(data);
-        console.log("=================");
-        console.log(api);
-        // const dataResponse  = ipcRenderer.sendSync('send-api-request', (event, api.apiEndPoint[0]))
+            var tbodyHeaders = document.getElementById("tbody-headers");
+            var tbodyBodys = document.getElementById("tbody-forms");
+            var oauthCheck = document.getElementsByName('authorize-type');
+            var methodRequest = document.getElementsByName("method-request");
+            var methodType = methodRequest[0].value;
         
-        // var dataResponse = null;
-        // var errorLog = document.getElementById('error-log');
-        ipcRenderer.send('send-api-request', (event, api))
+            var authValue = oauthCheck[0].value;
+            var isAuthentication = false;
+            var authContent = "";
+            if (authValue != "none") {
+                if ("oauth-2".localeCompare(authValue) == 0) authValue = "Bearer";
+                isAuthentication = true;
+                authContent = document.getElementsByName("auth-token")[0].value;
+                if (!authContent.includes("Bearer")) {
+                    authContent = authContent.trim();
+                    if (authContent.length > 1) {
+                        var authList = authContent.split(/\s/g);
+                        authContent = "";
+                        authList.forEach( function(element, index) {
+                            authContent += element;
+                        });
+                    }
+                }
+                else {
+                    authContent = authContent.substr("Bearer ".length - 1, authContent.length - 1);
+                    authContent = authContent.trim();
+                    if (authContent.length > 1) {
+                        var authList = authContent.split(/\s/g);
+                        authContent = "";
+                        authList.forEach( function(element, index) {
+                            authContent += element;
+                        });
+                    }
+                }
+            }
+        
+            // Authentication
+            var authen = {
+                type: authValue,
+                isAuthen: isAuthentication,
+                token: authContent
+            }
+        
+            // Header
+            var trHeadersParent = tbodyHeaders.childNodes;
+            var headersParams = {}
+            var countHeader = 0;
+            trHeadersParent.forEach( function(element, index) {
+                if (element.tagName == 'TR') {
+                    var param = {}
+                    var tdParams = element.childNodes;
+                    var isCheck = tdParams[1].childNodes[1].checked;
+                    var key = tdParams[3].childNodes[1].value;
+                    var value = tdParams[5].childNodes[1].value;
+                    var description = tdParams[7].childNodes[1].value;
+                    if (isCheck == true) {
+                        headersParams[countHeader] = {
+                            isCheck: isCheck,
+                            key: key,
+                            value: value,
+                            description: description
+                        }
+                        countHeader++;
+                    }
+                }
+            });
+            if (countHeader == 0) headersParams = {};
+        
+            // Body
+            var checkValueForm = "none";
+            var formCheck = document.getElementsByName('body-form');
+            formCheck.forEach( function(element, index) {
+                if (element.checked) {
+                    checkValueForm = element.value;
+                }
+            });
+            var body = {}
+            if (checkValueForm != "none") {
+                if (checkValueForm == "form-data") {
+                    body.type = checkValueForm;
+                    var trBodyParent = tbodyBodys.childNodes;
+                    var countBody = 0;
+                    trBodyParent.forEach( function(element, index) {
+                        if (element.tagName == 'TR') {
+                            var tdParams = element.childNodes;
+                            var isCheck = tdParams[1].childNodes[1].checked;
+                            var key = tdParams[3].childNodes[1].value;
+                            var value = tdParams[5].childNodes[1].value;
+                            var description = tdParams[7].childNodes[1].value;
+                            if (isCheck == true) {
+                                body[countBody] = {
+                                    isCheck: isCheck,
+                                    key: key,
+                                    value: value,
+                                    description: description
+                                }
+                                countBody++;
+                            }
+                        }
+                    });
+                    if (countBody == 0) body = {};
+                }
+                else if (checkValueForm == "raw"){
+                    body.type = checkValueForm;
+                    
+                }
+            }
+        
+            var data = { 
+                Req: {
+                    Url: url,
+                    Authen: authen,
+                    Headers: headersParams,
+                    Body: body,
+                    Type: methodType
+                }
+            }
+        
+            // console.log(data);
+        
+            // Test preload response => 
+            // setTimeout(function() {
+            //     titleResponse.style.display = 'block';
+            //     gifPreload.style.display = 'none';
+            //     var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
+            //     preloadResponse.style.display = 'none';
+            // }, 1000);
+        
+            // Change above code by below code
+        
+            // titleResponse.style.display = 'block';
+            // gifPreload.style.display = 'none';
+            // var preloadResponse = document.getElementsByClassName('pre-load-response')[0];
+            // preloadResponse.style.display = 'none';
+        
+        
+            // Test show response data 
+        
+            // var response = {
+            // 	header: {
+            // 		0: {
+            // 			key: "Content-type",
+            // 			value: "application/json"
+            // 		},
+            // 		1: {
+            // 			key: "Content-type",
+            // 			value: "application/json"
+            // 		},
+            // 		2: {
+            // 			key: "Content-type",
+            // 			value: "application/json"
+            // 		},
+            // 	},
+            // 	time: 100,
+            // 	size: 100,
+            // 	statusCode: 200,
+            // 	data: {data:"fa", fa:"data"},
+            // 	request: ""
+            // }
+            // responseData(response);
+            
+            const date = dateFormat(new Date(), "yyyy-mm-dd");
+            const idApi = dateFormat(new Date(), "yyyymmddHHMMssL");
+            const timeRequest = dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss:L"); //2020-06-01 15:04:16:09 -> example
+            //const isOpen = '' //TODO: WAITING UI is finish.
+            //const api = ''//TODO: WAITING UI is finish.
+            //const method = ''//TODO: WAITING UI is finish.
+            let api = {
+                date: date,
+                isOpen: true,
+                apiEndPoint: [{
+                    id: idApi,
+                    timeRequest: timeRequest,
+                    url: data.Req.Url,
+                    method: data.Req.Type,
+                    headers: data.Req.Headers,
+                    authenticate: data.Req.Authen,
+                    body: data.Req.Body
+                }],
+            }
+            //
+            console.log(data);
+            console.log("=================");
+            console.log(api);
+            // const dataResponse  = ipcRenderer.sendSync('send-api-request', (event, api.apiEndPoint[0]))
+            
+            // var dataResponse = null;
+            // var errorLog = document.getElementById('error-log');
+            ipcRenderer.send('send-api-request', (event, api))
+        })
     })
+    
 
     /**
      * GET all request data by api-data-id when click li element
@@ -348,7 +352,6 @@ window.addEventListener('click', () => {
     ipcRenderer.on('response-data-in-menu-history', (event, apiRequested) => {
         console.log(apiRequested);
         addTabItem(apiRequested);
-        
     })
 
     /**
